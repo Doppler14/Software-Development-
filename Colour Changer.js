1ae97d035c4aa6372e47297570e29a7c11858f61
David Ugwu <136579954+Doppler14@users.noreply.github.com>
2024-04-25 13:42:02 +0100

    Add files via upload

---
+++ Colour Changer.js
@@ -0,0 +1,18 @@
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const colorBox = document.getElementById("color-box");
+    const changeColorBtn = document.getElementById("change-color-btn");
+
+    changeColorBtn.addEventListener("click", function() {
+        let randomColor = getRandomColor();
+        colorBox.style.backgroundColor = randomColor;
+    });
+});
